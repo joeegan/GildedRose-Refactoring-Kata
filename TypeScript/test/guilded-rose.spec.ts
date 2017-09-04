23885a97af9dfd2216f2e778ff4af8fd91c908d6
Joe Egan <joeegan84@gmail.com>
2017-09-04 17:21:56 +0100

    begin asserting current assumptions

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,12 +1,24 @@
 import { expect } from 'chai'
 import { Item, GildedRose } from '../app/gilded-rose'
 
-describe('Gilded Rose', function() {
-  it('should foo', function() {
-    const gildedRose = new GildedRose([
-      new Item('foo', 0, 0),
+describe('Items', () => {
+  it('Have a name string, sellIn number, and a quality number', () => {
+    const inventory = new GildedRose([
+      new Item('foo', 1, 2),
     ])
-    const items = gildedRose.updateQuality()
+    const items = inventory.items
+    expect(items.length).to.equal(1)
     expect(items[0].name).to.equal('foo')
+    expect(items[0].sellIn).to.equal(1)
+    expect(items[0].quality).to.equal(2)
+  })
+
+  it('Decrement their sellIn and quality every time updateQuality is called', () => {
+    const inventory = new GildedRose([
+      new Item('foo', 1, 2),
+    ])
+    const items = inventory.updateQuality()
+    expect(items[0].sellIn).to.equal(0)
+    expect(items[0].quality).to.equal(1)
   })
 })
